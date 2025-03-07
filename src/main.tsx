b5b8650ab768545eaf76daf29c603f9a54fda09b
Marina.S. <marina.sevostian@gmail.com>
2025-03-07 16:22:53 +0200

    fixed main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
 import App from './components/App/App';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
